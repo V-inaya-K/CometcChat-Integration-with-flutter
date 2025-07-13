86612cd818245cded4cda952a0008aa21f70e4e5
Vinayak Shukla <98159594+V-inaya-K@users.noreply.github.com>
2025-07-13 12:51:22 +0530

    Update cometchat_config.dart

--- lib/cometchat_config.dart
+++ lib/cometchat_config.dart
@@ -1,5 +1,5 @@
 class CometChatConfig {
-  static const String appId = "27882548524bd106"; // Replace with your App ID
-  static const String region = "IN"; // Replace with your App Region
-  static const String authKey = "87ceb635951f77c1529500f78bfb1611bb6c9c8c"; // Replace with your Auth Key
+  static const String appId = "APP_ID"; // Replace with your App ID
+  static const String region = "REGION"; // Replace with your App Region
+  static const String authKey = "AUTH_KEY"; // Replace with your Auth Key
 }
